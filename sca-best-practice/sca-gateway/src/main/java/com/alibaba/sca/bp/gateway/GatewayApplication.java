330a7337b8d93a041f6feba5c670f7b1fb2c342f
lenny <niele@corp.netease.com>
2019-01-29 11:51:47 +0800

    nacos通过service 负载均衡

--- sca-best-practice/sca-gateway/src/main/java/com/alibaba/sca/bp/gateway/GatewayApplication.java
+++ sca-best-practice/sca-gateway/src/main/java/com/alibaba/sca/bp/gateway/GatewayApplication.java
@@ -18,11 +18,13 @@ package com.alibaba.sca.bp.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author xiaolongzuo
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
